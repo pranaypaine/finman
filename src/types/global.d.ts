d0a75c5b82c10ea98d2a2caa509de079ae37542d
Pranay Kumar Paine <pranaykumarpaine@gmail.com>
2026-03-08 14:59:51 +0000

    Fix TypeScript configuration and add type declarations

---
+++ src/types/global.d.ts
@@ -0,0 +1,18 @@
+// Type declarations for React Native global types
+
+declare const __DEV__: boolean;
+
+declare module '*.png' {
+  const value: any;
+  export default value;
+}
+
+declare module '*.jpg' {
+  const value: any;
+  export default value;
+}
+
+declare module '*.json' {
+  const value: any;
+  export default value;
+}
